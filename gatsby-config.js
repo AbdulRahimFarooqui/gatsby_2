2ec765012068d9665ec0836a405d5c8f789e2adb
AbdulRahimFarooqui <67377097+AbdulRahimFarooqui@users.noreply.github.com>
2021-06-22 10:01:10 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
   {
     resolve: `gatsby-source-contentful`,
     options: {
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      spaceId: ${{secrets.CONTENTFUL_SPACE_ID}},
       // Learn about environment variables: https://gatsby.dev/env-vars
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      accessToken: ${{secrets.CONTENTFUL_TOKEN}},
     },
   },
 
